e16f51433b224db4565dcb71cdac0097cab42fdc
its-vedu <vedantm606@gmail.com>
2024-09-10 00:18:07 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-// require('dotenv').config({path:'./env'})
-
-
-// import mongoose from "mongoose";
-// import { DB_NAME} from "./constants"
+import { app } from "./app.js"
 import  connectDB  from "./db/index.js"
 
 import dotenv from "dotenv"
@@ -10,6 +6,18 @@ dotenv.config({path:'./env'})
 
 
 connectDB()
+.then(()=>{
+  app.on("error",(error)=>{
+    console.error("Error",error)
+    throw error
+  })
+  app.listen(process.env.PORT||8000,()=>{
+    console.log(`server is running on port ${process.env.PORT}`)
+  })
+})
+.catch((err)=>{
+  console.log("MongoDB connection failed !!!!!",err)
+})
 
 
 
